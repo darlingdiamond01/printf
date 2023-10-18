d8b3c8c8adca3c0a4188063fce654088484d7afd
OKPALA PERPETUA CHIOMA <141235581+Perpy93@users.noreply.github.com>
2023-10-17 21:12:32 -0700

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed
+ * get_size - Calculates size to cast the argument
+ * @format: Formatted string in which to print the arguments.
+ * @i: argument list to be printed
  *
  * Return: Precision.
  */
